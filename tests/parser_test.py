9d082e016d0a9568a9df3085d72549a2a6f49114
Jake Fenton <jake@fenton.io>
2017-11-08 10:16:16 -0500

    moved unit tests into a class

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -14,25 +14,24 @@ def get_all_tokens(given_lexer):
     return tokens
 
 
-def test_parser_system_include_example():
-    test_lines = open('tests/test_data/system_include.cpp', 'r').readlines()
-    parse_tree = parser.parse("\n".join(test_lines))
-    print(parse_tree, file=sys.stderr)
-
-
-def test_parser_file_include_example():
-    test_lines = open('tests/test_data/file_include.cpp', 'r').readlines()
-    parse_tree = parser.parse("\n".join(test_lines))
-    print(parse_tree, file=sys.stderr)
-
-
-def test_parser_error():
-    test_lines = open('tests/test_data/error.cpp', 'r').readlines()
-    exception_caught = False
-    parse_tree = None
-    try:
+class TestUnit(object):
+    def test_parser_system_include_example(self):
+        test_lines = open('tests/test_data/system_include.cpp', 'r').readlines()
         parse_tree = parser.parse("\n".join(test_lines))
-    except Exception:
-        exception_caught = True
-    assert(exception_caught)
-    print(parse_tree, file=sys.stderr)
+        print(parse_tree, file=sys.stderr)
+
+    def test_parser_file_include_example(self):
+        test_lines = open('tests/test_data/file_include.cpp', 'r').readlines()
+        parse_tree = parser.parse("\n".join(test_lines))
+        print(parse_tree, file=sys.stderr)
+
+    def test_parser_error(self):
+        test_lines = open('tests/test_data/error.cpp', 'r').readlines()
+        exception_caught = False
+        parse_tree = None
+        try:
+            parse_tree = parser.parse("\n".join(test_lines))
+        except Exception:
+            exception_caught = True
+        assert(exception_caught)
+        print(parse_tree, file=sys.stderr)
