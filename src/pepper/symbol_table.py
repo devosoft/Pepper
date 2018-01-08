be6e04a0898d4157a4dedb9df92020510d7d79bd
Jake Fenton <jake@fenton.io>
2018-01-08 09:57:31 -0500

    expanding symbol table to have macros

--- src/pepper/symbol_table.py
+++ src/pepper/symbol_table.py
@@ -3,6 +3,22 @@ The Symbol Table module implements a class to track declarations and usages of i
 """
 
 
+class MacroExpansion():
+    def __init__(self, name, expansion, args=[]):
+        self.name = name
+        self.expansion = expansion
+        self.args = args
+
+    def expand(self, args=[]):
+        if len(args) != len(self.args):
+            raise SyntaxError(f"Wrong number of arguments in macro expansion for {self.name};"
+                              f" expected {len(self.args)}, got {len(args)}")
+        expansion = self.expansion
+        for index, arg in enumerate(args):
+            expansion = expansion.replace(self.args[index], str(arg))
+        return expansion
+
+
 class SymbolTable():
     def __init__(self):
         self.table = dict()
